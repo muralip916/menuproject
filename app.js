f6d3755528277ec41a35096896a040895d8c5d6a
muralip916 <110754246+muralip916@users.noreply.github.com>
2022-09-10 14:12:59 +0530

    Add files via upload
    I uploaded the all files relate to the project

---
+++ app.js
@@ -0,0 +1,322 @@
+const menu = [
+    
+    {
+        id: 1,
+        title: 'BIRIYANI MASALA',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'biryanimasala.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',
+    },
+    {
+        id: 2,
+        title: 'CHANA MASALA',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'chanamasala.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 3,
+        title: 'CHETTINADU MASALA',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'chettinadumasala.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 4,
+        title: 'CHICKEN MASALA',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'chickenmasala.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 5,
+        title: 'CHILLI MASALA',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'chillipowder.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 6,
+        title: 'CORIANDER MASALA',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'coriandarmasala.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 7,
+        title: 'CUMIN POWDER',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'cuminpowder.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+       {
+        id: 8,
+        title: 'CURRY MASALA',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'currymasala.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 9,
+        title: 'DINDIGUL BIRYANI MASALA',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'dindigulbiryanimasala.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 10,
+        title: 'GARAM MASALA',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'garammasala.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 11,
+        title: 'HYDERABAD BIRIYANI MASALA',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'hyderabadibiryanimasala.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 12,
+        title: 'KASHMIRI CHILLI POWDER',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'kashmirichillipowder.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 13,
+        title: 'MUTTON MASALA',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'muttonmasala.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 14,
+        title: 'PEPPER POWDER',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'pepperpowder.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 15,
+        title: 'TURMERIC POWDER',
+        category :'MASALA ITEMS',
+        price : 55,
+        img: 'turmericpowder.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+
+    {
+        id: 16,
+        title: 'AMLA PICKLE',
+        category :'PICKLE ITEMS',
+        price : 55,
+        img: 'Amla Pickle.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 17,
+        title: 'CITRON PICKLE',
+        category :'PICKLE ITEMS',
+        price : 55,
+        img: 'citron pickle.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 18,
+        title: 'CUT MANGO PICKLE',
+        category :'PICKLE ITEMS',
+        price : 55,
+        img: 'cut mango Pickle.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',  
+    },
+    {
+        id: 19,
+        title: 'GARLIC PICKLE',
+        category :'PICKLE ITEMS',
+        price : 55,
+        img: 'GARLIC PICKLE.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',   
+    },
+    {
+        id: 20,
+        title: 'GINGER GARLIC',
+        category :'PICKLE ITEMS',
+        price : 55,
+        img: 'ginger pickle.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',   
+    },
+    {
+        id: 21,
+        title: 'GINGER GARLIC PICKLE',
+        category :'PICKLE ITEMS',
+        price : 55,
+        img: 'ginger garlic pickle.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 22,
+        title: 'LIME PICKLE',
+        category :'PICKLE ITEMS',
+        price : 55,
+        img: 'Lime Pickle.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 23,
+        title: 'MANGO GINGER PICKLE',
+        category :'PICKLE ITEMS',
+        price : 55,
+        img: 'mango ginger pickle.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 24,
+        title: 'MIXED VEG PICKLE',
+        category :'PICKLE ITEMS',
+        price : 55,
+        img: 'Mixed Veg Pickle.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 25,
+        title: 'MANGO THOKKU PICKLE',
+        category :'PICKLE ITEMS',
+        price : 55,
+        img: 'MANGO THOKKU PICKLE.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 26,
+        title: 'TOMATO GARLIC PICKLE',
+        category :'PICKLE ITEMS',
+        price : 55,
+        img: 'garlic_tomato_thokku.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 27,
+        title: 'LEMON RICE PASTE',
+        category :'RICE PASTE ITEMS',
+        price : 55,
+        img: 'Lemonricepaste.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',   
+    },
+    {
+        id: 28,
+        title: 'PUDHINA RICE PASTE',
+        category :'RICE PASTE ITEMS',
+        price : 55,
+        img: 'Pudhinaricepaste.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 29,
+        title: 'PULIYOTHARAI RICE PASTE',
+        category :'RICE PASTE ITEMS',
+        price : 55,
+        img: 'puliyotharairicepaste.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 30,
+        title: 'TOMATO RICE PASTE',
+        category :'RICE PASTE ITEMS',
+        price : 55,
+        img: 'toamtoricepaste.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+    {
+        id: 31,
+        title: 'VATHA KOZAMBU RICE PASTE',
+        category :'RICE PASTE ITEMS',
+        price : 55,
+        img: 'vaththakulamburicepaste.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, numquam eligendi. Neque laudantium non quasi officia voluptatem culpa inventore?',    
+    },
+];
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {
+  diplayMenuItems(menu);
+  displayMenuButtons();
+});
+
+function diplayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
+
+    return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  // console.log(displayMenu);
+
+  sectionCenter.innerHTML = displayMenu;
+}
+function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button type="button" class="filter-btn" data-id=${category}>
+          ${category}
+        </button>`;
+    })
+    .join("");
+
+  btnContainer.innerHTML = categoryBtns;
+  const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+  console.log(filterBtns);
+
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+       console.log(e.currentTarget.dataset);
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        // console.log(menuItem.category);
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "all") {
+        diplayMenuItems(menu);
+      } else {
+        diplayMenuItems(menuCategory);
+      }
+    });
+  });
+}
---
+++ kkagencyitem.js
@@ -0,0 +1,553 @@
+const groceryItem= [
+    {
+        id: 1,
+        name: 'ALL-OUT ',
+        category :'ALL OUT ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'alloutimage.jpg',
+        quantity : 1    
+    },
+    {
+        id: 2,
+        name: 'All-OUT REFILL',
+        category :'ALL OUT ITEMS',
+        rating : 4.3,
+        price : 49,
+        img: 'alloutrefill.png',
+        quantity : 1    
+    },
+    {
+        id: 3,
+        name: 'ALL OUT COIL',
+        category :'ALL OUT ITEMS',
+        rating : 4.3,
+        price : 30,
+        img: 'alloutcoil.jpg',
+        quantity : 1    
+    },
+    {
+        id: 4,
+        name: 'ADHIRASAM',
+        category :'ADHIRASAM',
+        rating : 4.3,
+        price : 52.90,
+        img: 'adhirasam.jpg',
+        quantity : 1    
+    },
+    {
+        id: 5,
+        name: 'APPALAM',
+        category :'APPALAM',
+        rating : 4.3,
+        price : 18,
+        img: 'appalam.jpg',
+        quantity : 1    
+    },
+    {
+        id: 6,
+        name: 'ATTA',
+        category :'ATTA',
+        rating : 4.3,
+        price : 17,
+        img: 'atta.jpg',
+        quantity : 1    
+    },
+    {
+        id: 7,
+        name: 'POWDER',
+        category :'ASAFOETIDA',
+        rating : 4.3,
+        price : 85,
+        img: 'asafeotdiapowder.jpg',
+        quantity : 1    
+    },
+    {
+        id: 8,
+        name: 'CAKE',
+        category :'ASAFOETIDA',
+        rating : 4.3,
+        price : 35,
+        img: 'Asafotidacake.jpg',
+        quantity : 1    
+    },
+    {
+        id: 9,
+        name: 'CONTAINER',
+        category :'ASAFOETIDA',
+        rating : 4.3,
+        price : 48,
+        img: 'asafeotdiapowder.jpg',
+        quantity : 1    
+    },
+    {
+        id: 10,
+        name: 'BADAM MIX',
+        category :'BADAM MIX',
+        rating : 4.3,
+        price : 83,
+        img: 'badammix.jpg',
+        quantity : 1    
+    },
+    {
+        id: 11,
+        name: 'BAJJI',
+        category :'BAJJI',
+        rating : 4.3,
+        price : 55,
+        img: 'bajji.jpg',
+        quantity : 1    
+    },
+    {
+        id: 12,
+        name: 'BHEL PURI',
+        category :'BHEL PURI',
+        rating : 4.3,
+        price : 55,
+        img: 'bhelpuri.jpg',
+        quantity : 1    
+    },
+    {
+        id: 13,
+        name: 'BIRIYANI MASALA',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'biryanimasala.jpg',
+        quantity : 1    
+    },
+    {
+        id: 14,
+        name: 'CHANA MASALA',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'chanamasala.jpg',
+        quantity : 1    
+    },
+    {
+        id: 15,
+        name: 'CHETTINADU MASALA',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'chettinadumasala.jpg',
+        quantity : 1    
+    },
+    {
+        id: 16,
+        name: 'CHICKEN MASALA',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'chickenmasala.jpg',
+        quantity : 1    
+    },
+    {
+        id: 17,
+        name: 'CHILLI MASALA',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'chillipowder.jpg',
+        quantity : 1    
+    },
+    {
+        id: 18,
+        name: 'CORIANDER MASALA',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'coriandarmasala.jpg',
+        quantity : 1    
+    },
+    {
+        id: 19,
+        name: 'CUMIN POWDER',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'cuminpowder.jpg',
+        quantity : 1    
+    },
+       {
+        id: 20,
+        name: 'CURRY MASALA',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'currymasala.jpg',
+        quantity : 1    
+    },
+    {
+        id: 21,
+        name: 'DINDIGUL BIRYANI MASALA',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'dindigulbiryanimasala.jpg',
+        quantity : 1    
+    },
+    {
+        id: 22,
+        name: 'GARAM MASALA',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'garammasala.jpg',
+        quantity : 1    
+    },
+    {
+        id: 23,
+        name: 'HYDERABAD BIRIYANI MASALA',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'hyderabadibiryanimasala.jpg',
+        quantity : 1    
+    },
+    {
+        id: 24,
+        name: 'KASHMIRI CHILLI POWDER',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'kashmirichillipowder.jpg',
+        quantity : 1    
+    },
+    {
+        id: 25,
+        name: 'MUTTON MASALA',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'muttonmasala.jpg',
+        quantity : 1    
+    },
+    {
+        id: 26,
+        name: 'PEPPER POWDER',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'pepperpowder.jpg',
+        quantity : 1    
+    },
+    {
+        id: 27,
+        name: 'TURMERIC POWDER',
+        category :'MASALA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'turmericpowder.jpg',
+        quantity : 1    
+    },
+    {
+        id: 28,
+        name: 'CHETTINADU BIRYANI KIT',
+        category :'BIRIYANI KII',
+        rating : 4.3,
+        price : 55,
+        img: 'chettinadubiryanikit.jpg',
+        quantity : 1    
+    },
+    {
+        id: 29,
+        name: 'DINDIGUL BIRYANI KIT',
+        category :'BIRIYANI KII',
+        rating : 4.3,
+        price : 55,
+        img: 'dindigulbiriyanikit.jpg',
+        quantity : 1   
+    },
+    {
+        id: 30,
+        name: 'HYDERABAD BIRYANI KIT',
+        category :'BIRIYANI KII',
+        rating : 4.3,
+        price : 55,
+        img: 'Hyderabadibiryanikit.jpg',
+        quantity : 1    
+    },
+    {
+        id: 31,
+        name: 'DAL PAYASAM',
+        category :'DAL PAYASAM',
+        rating : 4.3,
+        price : 55,
+        img: 'dalpayasam.jpg',
+        quantity : 1    
+    },
+    {
+        id: 32,
+        name: 'GHEE JAR',
+        category :'GHEE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'gheejar.jpg',
+        quantity : 1    
+    },
+    {
+        id: 33,
+        name: 'GHEE POUCH',
+        category :'GHEE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'gheepouch.jpg',
+        quantity : 1    
+    },
+    {
+        id: 34,
+        name: 'GULAB JAMUN MIX',
+        category :'GULAB JAMUN',
+        rating : 4.3,
+        price : 55,
+        img: 'gulabjamunmix.jpg',
+        quantity : 1    
+    },
+    {
+        id: 35,
+        name: 'HEALTH MIX',
+        category :'HEALTH MIX',
+        rating : 4.3,
+        price : 55,
+        img: 'healthmix.jpg',
+        quantity : 1    
+    },
+    {
+        id: 36,
+        name: 'KOZHUKATTAI',
+        category :'KOZHUKATTAI',
+        rating : 4.3,
+        price : 55,
+        img: 'Kozhukattai.jpg',
+        quantity : 1    
+    },
+    {
+        id: 37,
+        name: 'LG ASAFOETIDA CAKE',
+        category :'LG ASAFOETIDA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'lgasafoetidacake.jpg',
+        quantity : 1    
+    },
+    {
+        id: 38,
+        name: 'LG ASAFOETIDA POWDER',
+        category :'LG ASAFOETIDA ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'lgasafoetidapowder.jpg',
+        quantity : 1    
+    },
+    {
+        id: 39,
+        name: 'MIXED FRUIT JAM',
+        category :'MIXED FRUIT JAM',
+        rating : 4.3,
+        price : 55,
+        img: 'Mixed Fruit Jam.jpg',
+        quantity : 1    
+    },
+    {
+        id: 40,
+        name: 'PANI PURI KIT',
+        category :'PANI PURI KIT',
+        rating : 4.3,
+        price : 55,
+        img: 'panipurikit.jpg',
+        quantity : 1   
+    },
+    {
+        id: 41,
+        name: 'PAYASAM MIX',
+        category :'PAYASAM MIX',
+        rating : 4.3,
+        price : 55,
+        img: 'payasammix.jpg',
+        quantity : 1    
+    },
+    {
+        id: 42,
+        name: 'AMLA PICKLE',
+        category :'PICKLE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'Amla Pickle.jpg',
+        quantity : 1    
+    },
+    {
+        id: 43,
+        name: 'CITRON PICKLE',
+        category :'PICKLE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'citron pickle.jpg',
+        quantity : 1    
+    },
+    {
+        id: 44,
+        name: 'CUT MANGO PICKLE',
+        category :'PICKLE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'cut mango Pickle.jpg',
+        quantity : 1  
+    },
+    {
+        id: 45,
+        name: 'GARLIC PICKLE',
+        category :'PICKLE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'GARLIC PICKLE.jpg',
+        quantity : 1   
+    },
+    {
+        id: 46,
+        name: 'GINGER GARLIC',
+        category :'PICKLE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'ginger pickle.jpg',
+        quantity : 1   
+    },
+    {
+        id: 47,
+        name: 'GINGER GARLIC PICKLE',
+        category :'PICKLE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'ginger garlic pickle.jpg',
+        quantity : 1    
+    },
+    {
+        id: 48,
+        name: 'LIME PICKLE',
+        category :'PICKLE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'Lime Pickle.jpg',
+        quantity : 1    
+    },
+    {
+        id: 49,
+        name: 'MANGO GINGER PICKLE',
+        category :'PICKLE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'mango ginger pickle.jpg',
+        quantity : 1    
+    },
+    {
+        id: 50,
+        name: 'MIXED VEG PICKLE',
+        category :'PICKLE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'Mixed Veg Pickle.jpg',
+        quantity : 1    
+    },
+    {
+        id: 51,
+        name: 'MANGO THOKKU PICKLE',
+        category :'PICKLE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'MANGO THOKKU PICKLE.jpg',
+        quantity : 1    
+    },
+    {
+        id: 52,
+        name: 'TOMATO GARLIC PICKLE',
+        category :'PICKLE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'garlic_tomato_thokku.jpg',
+        quantity : 1    
+    },
+    {
+        id: 53,
+        name: 'RAGI FLOUR',
+        category :'RAGI FLOUR',
+        rating : 4.3,
+        price : 55,
+        img: 'RagiFlour.jpg',
+        quantity : 1    
+    },
+    {
+        id: 54,
+        name: 'RAGI SEMIYA',
+        category :'RAGI SEMIYA',
+        rating : 4.3,
+        price : 55,
+        img: 'ragisemiya.jpg',
+        quantity : 1    
+    },
+    {
+        id: 55,
+        name: 'LEMON RICE PASTE',
+        category :'RICE PASTE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'Lemonricepaste.jpg',
+        quantity : 1   
+    },
+    {
+        id: 56,
+        name: 'PUDHINA RICE PASTE',
+        category :'RICE PASTE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'Pudhinaricepaste.jpg',
+        quantity : 1    
+    },
+    {
+        id: 57,
+        name: 'PULIYOTHARAI RICE PASTE',
+        category :'RICE PASTE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'puliyotharairicepaste.jpg',
+        quantity : 1    
+    },
+    {
+        id: 58,
+        name: 'TOMATO RICE PASTE',
+        category :'RICE PASTE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'toamtoricepaste.jpg',
+        quantity : 1    
+    },
+    {
+        id: 59,
+        name: 'VATHA KOZAMBU RICE PASTE',
+        category :'RICE PASTE ITEMS',
+        rating : 4.3,
+        price : 55,
+        img: 'vaththakulamburicepaste.jpg',
+        quantity : 1    
+    },
+         {
+        id: 60,
+        name: 'SAMBA RICE',
+        category :'SAMBA RICE',
+        rating : 4.3,
+        price : 55,
+        img: 'samba.jpg',
+        quantity : 1    
+    },
+    {
+        id: 61,
+        name: 'SEMIYA',
+        category :'SEMIYA',
+        rating : 4.3,
+        price : 55,
+        img: 'Semiyapasyammix.jpg',
+        quantity : 1    
+    }
+]
+
+export {groceryItem};
